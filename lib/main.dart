923de4630c2ce371a4ed41faa8e1adbee6bf33c2
Althea Giron <altheanoreen.giron@ctx.edu>
2024-03-14 18:48:13 -0500

    beginning of home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:lets_get_it_done/screens/intro_screen.dart';
-import 'package:lets_get_it_done/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,14 +12,35 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  DateTime today = DateTime.now();
+  late Map<DateTime, List<Task>> selectedTasks;
 
-  void _onDaySelected(DateTime day, DateTime focusedDay) {
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
+
+  TextEditingController _taskController = TextEditingController();
+
+  @override
+  void initState() {
+    selectedTasks = {};
+    super.initState();
+  }
+
+  void dispose() {
+    _taskController.dispose();
+    super.dispose();
+  }
+
+  List<Task> _getTasksfromDay(DateTime date) {
+    return selectedTasks[date] ?? [];
+  }
+
+  void _onDaySelected(DateTime selectDay, DateTime focusDay) {
       setState(() {
-       today = day;
+       selectedDay = selectDay;
+       focusedDay = focusDay;
       });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,15 +77,69 @@ class _HomeScreenState extends State<HomeScreen> {
                 shape: BoxShape.circle,
               ),
             ),
-            selectedDayPredicate: (day) => isSameDay(day, today),
-            focusedDay: today,
+            selectedDayPredicate: (DateTime date) => isSameDay(selectedDay, date),
+            focusedDay: selectedDay,
             firstDay: DateTime.utc(2024, 3, 14),
             lastDay: DateTime.utc(2025, 3, 14),
             onDaySelected: _onDaySelected,
             calendarFormat: CalendarFormat.week,
+
+            eventLoader: _getTasksfromDay,
+
           ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            child: const Text('To-Do:',
+            style: TextStyle(
+              color: Color(0xffcebb9c),
+              fontSize: 20,
+              )
+            ),
+          ),
+          ..._getTasksfromDay(selectedDay).map((Task task) => ListTile(title: Text(task.title),),),
         ]  
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showDialog(
+          context: context, 
+          builder: (context) => AlertDialog(
+            title: const Text("Add Task"),
+            content: TextFormField(
+              controller: _taskController,
+            ),
+            actions: [
+              TextButton(
+                child: const Text("Add"),
+                onPressed: () {
+                  if(_taskController.text.isEmpty) {
+            
+                  }
+                  else {
+                    if(selectedTasks[selectedDay] != null) {
+                      selectedTasks[selectedDay]?.add(
+                        Task(title: _taskController.text),
+                      );
+                    }
+                    else {
+                      selectedTasks[selectedDay] = [
+                        Task(title: _taskController.text)
+                      ];
+                    }
+                    
+                  }
+                  Navigator.pop(context);
+                  _taskController.clear();
+                  setState(() {});
+                  return;
+                }
+                
+              )
+            ]
+          )),
+        child: const Icon(Icons.add),
+        ),
     );
   }
 }
